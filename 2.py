4b9f2822da3a705a42c13abf1ab9cce2c24b4fb0
Josh Zucker <zuckerje@gmail.com>
2018-12-02 09:13:49 -0500

    day 2

---
+++ 2.py
@@ -0,0 +1,36 @@
+def has_repetition(word, num):
+    counts = {}
+    for w in word:
+        counts[w] = counts[w] + 1 if w in counts else 1
+    return num in counts.values()
+
+
+def p1(words):
+    has_two = sum(map(lambda x: has_repetition(x, 2), words))
+    has_three = sum(map(lambda x: has_repetition(x, 3), words))
+    return has_two * has_three
+
+
+def p2(words):
+    for w1 in words:
+        for w2 in words:
+            if diff_count(w1, w2) == 1:
+                return common(w1, w2)
+
+
+def diff_count(w1, w2):
+    return sum(map(lambda x: x[0] != x[1], list(zip(w1, w2))))
+
+
+def common(w1, w2):
+    cmn = filter(lambda x: x[0] == x[1], list(zip(w1, w2)))
+    return "".join(map(lambda x: x[0], cmn))
+
+
+if __name__ == "__main__":
+    import sys
+    import utils
+    part = sys.argv[1]
+    words = utils.file_to_1d_array('inputs/2.txt')
+    answer = p1(words) if part == '1' else p2(words)
+    print(answer)
