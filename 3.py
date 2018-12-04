1e29f431b59459a7c95a14ce69c17e752acea3cb
Josh Zucker <zuckerje@gmail.com>
2018-12-03 20:49:48 -0500

    day3

---
+++ 3.py
@@ -0,0 +1,61 @@
+import re
+from collections import Counter
+
+
+def p1(claims):
+    mp = build_map(claims)
+    return sum(len(v) > 1 for v in mp.values())
+
+
+def build_map(claims):
+    mp = {}
+    for claim in claims:
+        parsed = parse_claim(claim)
+        squares = get_covered_squares(parsed)
+        for square in squares:
+            if square in mp:
+                mp[square].append(parsed["id"])
+            else:
+                mp[square] = [parsed["id"]]
+    return mp
+
+
+def p2(lines):
+    mp = build_map(claims)
+    no_overlaps = set(
+        flat_list(list(filter(lambda x: len(x) == 1, mp.values()))))
+    overlaps = set(
+        flat_list(list(filter(lambda x: len(x) > 1, mp.values()))))
+    return no_overlaps - overlaps
+
+
+def parse_claim(claim):
+    match = re.match(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', claim)
+    return {
+        "id": int(match.group(1)),
+        "left": int(match.group(2)),
+        "top": int(match.group(3)),
+        "width": int(match.group(4)),
+        "height": int(match.group(5)),
+    }
+
+
+def get_covered_squares(claim):
+    return [serialize(x, y) for x in range(claim["left"], claim["left"] + claim["width"]) for y in range(claim["top"], claim["top"] + claim["height"])]
+
+
+def serialize(x, y):
+    return "{0}:{1}".format(x, y)
+
+
+def flat_list(lists):
+    return [item for sublist in lists for item in sublist]
+
+
+if __name__ == "__main__":
+    import sys
+    import utils
+    part = sys.argv[1]
+    claims = utils.file_to_1d_array('inputs/3.txt')
+    answer = p1(claims) if part == '1' else p2(claims)
+    print(answer)
--- utils.py
+++ utils.py
@@ -14,7 +14,3 @@ def file_to_2d_array(path):
             arr.append(line.split())
     f.close()
     return arr
-
-
-def flat_list(lists):
-    return [item for sublist in lists for item in sublist]
